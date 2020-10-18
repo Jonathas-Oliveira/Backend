1f7e8a2acd7bf788292c3d0fcd3f35461c010c19
Jonathas Oliveira <68708850+Jonathas-Oliveira@users.noreply.github.com>
2020-10-18 17:44:05 -0200

    Creating index.js

---
+++ src/index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const {uuid, isUuid} = require('uuidv4')
+const app = express()
+
+
+app.use(express.json())
+
+/**
+ * Tipos http:
+ * Query: Filtros e paginação
+ * Params: Parametros da requisição
+ * Body: Conteúdo da requisição
+ */
+
+/*
+*Middlware
+*/
+
+function LogRequest(resquest,response,next){
+    const {method,url} = resquest
+
+    console.log(`[${method}]: ${url}`)
+    return next()
+}
+
+function ValidateId(request,response,next){
+    const {id} = request.params
+
+    if(!isUuid(id)){
+        return response.status(400).json({error:'Invalid project ID!'})
+    }
+    return next()
+}
+
+app.use(LogRequest)
+
+const projects = []
+
+
+app.get('/projects', (request, response) => {
+    
+    return response.json(projects)
+
+
+})
+
+
+app.post('/projects',(request, response) =>{
+    
+    const {title,message} = request.body
+    const project = {id:uuid(),title:title,message:message}
+    projects.push(project)
+
+    return response.json(project)
+})
+
+app.put('/projects/:id',ValidateId,(request, response) =>{
+    const {title,message} = request.query
+    const {id} = request.params
+    const projectsIndex = projects.findIndex(project => project.id === id )
+
+    if ( projectsIndex < 0) response.status(400).json({error:"The project wasn't found." })
+    
+    const project = {
+        id,
+        title,
+        message,
+    }
+    projects[projectsIndex] = project
+    
+    return response.json(project)
+})
+
+app.delete('/projects/:id',ValidateId,(request,reponse) =>{
+    
+    const {id} = request.params
+    const projectsIndex = projects.findIndex(project => project.id === id )
+
+    projects.splice(projectsIndex,1)
+    return reponse.status(204).send()
+})
+
+app.listen(3000, () =>{
+    return console.log('Back-end started!')
+})
